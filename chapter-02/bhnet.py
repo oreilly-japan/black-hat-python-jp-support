55e9de4bd6a4a1c5806fadda9ddad05d206e9d77
Kazufumi AOKI <aoki.kazufumi@lab.ntt.co.jp>
2015-11-24 14:45:51 +0900

    Fix usage output of bhnet

--- chapter-02/bhnet.py
+++ chapter-02/bhnet.py
@@ -19,7 +19,7 @@ port               = 0
 def usage():
     print "BHP Net Tool"
     print
-    print "Usage: bhpnet.py -t target_host -p port"
+    print "Usage: bhnet.py -t target_host -p port"
     print "-l --listen              - listen on [host]:[port] for"
     print "                           incoming connections"
     print "-e --execute=file_to_run - execute the given file upon"
@@ -30,10 +30,10 @@ def usage():
     print
     print
     print "Examples: "
-    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -c"
-    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -u c:\\target.exe"
-    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -e \"cat /etc/passwd\""
-    print "echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135"
+    print "bhnet.py -t 192.168.0.1 -p 5555 -l -c"
+    print "bhnet.py -t 192.168.0.1 -p 5555 -l -u c:\\target.exe"
+    print "bhnet.py -t 192.168.0.1 -p 5555 -l -e \"cat /etc/passwd\""
+    print "echo 'ABCDEFGHI' | ./bhnet.py -t 192.168.11.12 -p 135"
     sys.exit(0)
 
 def main():
